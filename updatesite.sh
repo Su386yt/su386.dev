03c3aba14b4e9fd451791a08d48b183d266a8cea
Su386 <83100266+Su386yt@users.noreply.github.com>
2026-03-28 11:48:13 -0400

    Update updatesite.sh

--- updatesite.sh
+++ updatesite.sh
@@ -4,6 +4,6 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 REPO_DIR="/home/opc/su386.dev/compiled/"
 WEB_ROOT="/usr/share/nginx/html/"
 
-cd "$REPO_DIR" || {exit 1; }
+cd "$REPO_DIR" || { exit 1; }
 git pull
 sudo rsync -av --delete ./compiled/ "$WEB_ROOT"
